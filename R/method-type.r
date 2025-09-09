7bf3c6c125b35b71ccd0238e8643c9e80596a04d
HervePerdry <herve.perdry@gmail.com>
2025-09-09 17:06:45 +0200

    Methods 'type' and 'apply' (for mmatrix)

---
+++ R/method-type.r
@@ -0,0 +1,19 @@
+#' Type of a memory-mapped object
+#'
+#' @rdname type
+#' @name type
+#'
+#' @param x a memory mapped object
+#'
+#' @details Sends back the stored data type (currently "double", "float", "int" or "short").
+#'
+#' @examples x <- mvector("int", 6)
+#' type(x)
+#'
+#' @export
+setGeneric("type", function(x) standardGeneric("type"))
+
+#' @rdname type
+setMethod("type", c(x = "memoryMapped"), 
+  function(x) x@datatype
+)
